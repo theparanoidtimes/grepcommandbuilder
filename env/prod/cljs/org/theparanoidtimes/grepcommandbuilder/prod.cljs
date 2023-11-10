27b072ce2c805ca2ae6c0255fdab62a10c29a67d
Dejan Josifović <theparanoidtimes@posteo.net>
2023-11-10 15:00:34 +0100

    Introduce gh-pages branch

--- env/prod/cljs/org/theparanoidtimes/grepcommandbuilder/prod.cljs
+++
@@ -1,8 +0,0 @@
-(ns org.theparanoidtimes.grepcommandbuilder.prod
-  (:require
-   [org.theparanoidtimes.grepcommandbuilder.core :as core]))
-
-;; Ignore println statements in prod.
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
